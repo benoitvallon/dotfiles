e8cb97c824036486b5ebd763b24155db00051b40
Benoit VALLON <benoitvallon@gmail.com>
2015-02-14 18:37:18 +0100

    Cleans zshrc to mirror oh-my-zsh default file

---
+++ zsh/load-config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Load all zsh files
+typeset -U config_files
+config_files=($HOME/.dotfiles/**/*.zsh)
+
+for file in ${(M)config_files:#*/*.zsh}
+do
+  source $file
+done
+unset config_files
