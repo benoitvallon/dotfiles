18485cb8057936e85ff0ba9eb44c8b94754f3656
Benoit VALLON <benoitvallon@gmail.com>
2017-06-22 20:32:25 -0400

    Add oh-my-zsh install script

---
+++ oh-my-zsh/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# oh-my-zsh
+#
+
+# Check for oh-my-zsh
+echo "  Installing oh-my-zsh for you."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
