2ad432897802faef95cca50a45e19ef1b5fbf2d9
Benoit VALLON <benoitvallon@gmail.com>
2021-04-17 21:44:27 +0200

    Fix shell starting time

--- zsh/load-config.sh
+++ zsh/load-config.sh
@@ -2,7 +2,7 @@
 
 # Load all zsh files
 typeset -U config_files
-config_files=($HOME/.dotfiles/**/*.zsh)
+config_files=($HOME/.dotfiles/(**|^atom\.symlink)/*.zsh)
 
 for file in ${(M)config_files:#*/*.zsh}
 do
